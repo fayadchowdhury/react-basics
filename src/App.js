9876b6e8e6e9d9ac6a12ce885de11f037e12b11d
fayadchowdhury <fayad.chowdhury@gmail.com>
2024-01-25 02:09:35 -0800

    Info.js: Add first class component and export it default App.js: Use Info component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Hello } from './components/Hello';
+import Info from './components/Info';
 
 function App() {
   return (
     <div className="App">
       <Hello/> {/* Functional components that take no parameters can be placed as self-closing tags */}
+      <Info/>
     </div>
   );
 }
---
+++ src/components/Info.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+    render() {
+        return <h1>This is Fayad's first React app!</h1>
+    }
+}
+
+export default Info;
